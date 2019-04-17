dc9b511daeb48626a72949db61f451d51f41abe1
KrutoiOblom <47597751+KrutoiOblom@users.noreply.github.com>
2019-04-17 12:52:50 +0300

    Add files via upload

---
+++ Практическая работа №5/Практическая работа №5.2.cpp
@@ -0,0 +1,86 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void Selection_Sort(int *A, int A_size) {
+	int t, k;
+	for (int i = 0; i < A_size - 1; i++) {
+		t = A[i];
+		k = i;
+		for (int j = i + 1; j < A_size; j++)
+			if (A[j] < A[k]) k = j;
+		if (k != i) {
+			A[i] = A[k];
+			A[k] = t;
+		}
+	}
+}
+
+void Selection_Sort(double *A, int A_size) {
+	double t;
+	int k;
+	for (int i = 0; i < A_size - 1; i++) {
+		t = A[i];
+		k = i;
+		for (int j = i + 1; j < A_size; j++)
+			if (A[j] < A[k]) 
+				k = j;
+		if (k != i) {
+			A[i] = A[k];
+			A[k] = t;
+		}
+	}
+}
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+
+	int n, type;
+	cout << "Введите кол-во элементов: ";
+	cin >> n;
+	cout << "\nВыберите тип массива\n1 - Целый\n2 - Дробный" << endl;
+	cin >> type;
+	switch (type) {
+	case 1: {
+		int *A;
+		A = new int[n];
+		cout << "\nЗаполните массив" << endl;
+		for (int i = 0; i < n; i++) {
+			cout << "A[" << i << "] = ";
+			cin >> A[i];
+		}
+		cout << "\nВведённый массив:" << endl;
+		for (int i = 0; i < n; i++)
+			cout << A[i] << " ";
+		Selection_Sort(A, n);
+		cout << "\nОтсортированный массив:" << endl;
+		for (int i = 0; i < n; i++)
+			cout << A[i] << " ";
+		delete[]A;
+		break; }
+	case 2: {
+		double *A;
+		A = new double[n];
+		cout << "\nЗаполните массив" << endl;
+		for (int i = 0; i < n; i++) {
+			cout << "A[" << i << "] = ";
+			cin >> A[i];
+		}
+		cout << "\nВведённый массив:" << endl;
+		for (int i = 0; i < n; i++)
+			cout << A[i] << " ";
+		Selection_Sort(A, n);
+		cout << "\nОтсортированный массив:" << endl;
+		for (int i = 0; i < n; i++)
+			cout << A[i] << " ";
+		delete[]A;
+		break; }
+	default: {
+		cerr << "Ошибка: Неверный ввод." << endl << endl;
+		system("pause");
+		return 1; }
+	}
+	cout << endl << endl;
+	system("pause");
+	return 0;
+}
---
+++ Практическая работа №5/Практическая работа №5.4.cpp
@@ -0,0 +1,82 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int operation(int operat, int a, int b) {
+	int c;
+	switch (operat) {
+	case 1:
+		c = a + b; break;
+	case 2:
+		c = a - b; break;
+	case 3: 
+		c = a * b; break;
+	case 4:
+		c = a / b; break;
+	default:
+		break;
+	}
+	return c;
+}
+
+double operation(int operat, double a, double b) {
+	double c;
+	switch (operat) {
+	case 1:
+		c = a + b; break;
+	case 2:
+		c = a - b; break;
+	case 3:
+		c = a * b; break;
+	case 4:
+		c = a / b; break;
+	default:
+		break;
+	}
+	return c;
+}
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	
+	int type, operat;
+	cout << "Выберите тип данных, с каким хотите работать:\n1 - Целый\n2 - Дробный" << endl;
+	cin >> type;
+	if ((type != 1) && (type != 2)) {
+		cerr << "Ошибка: неверный ввод." << endl << endl;
+		system("pause");
+		return 1;
+	}
+	cout << "\nВыберите действие, которое хотите выполнить:\n1 - Сложение\n2 - Вычитание\n3 - Умножение\n4 - Деление" << endl;
+	cin >> operat;
+	if ((operat != 1) && (operat != 2) && (operat != 3) && (operat != 4)) {
+		cerr << "Ошибка: неверный ввод." << endl << endl;
+		system("pause");
+		return 1;
+	}
+	switch (type) {
+	case 1: {
+		int a, b, c;
+		cout << "\nВведите число a: ";
+		cin >> a;
+		cout << "Введите число b: ";
+		cin >> b;
+		c = operation(operat, a, b);
+		cout << "\nРезультат: " << c;
+		break; }
+	case 2: {
+		double a, b, c;
+		cout << "\nВведите число a: ";
+		cin >> a;
+		cout << "Введите число b: ";
+		cin >> b;
+		c = operation(operat, a, b);
+		cout << "\nРезультат: " << c; 
+		break; }
+	default:
+		break;
+	}
+	cout << endl << endl;
+	system("pause");
+	return 0;
+}
