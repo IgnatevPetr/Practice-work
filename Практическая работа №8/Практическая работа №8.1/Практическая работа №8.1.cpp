f8cc48285aab3de1d6d8f5c8cf4465c0fad336e2
KrutoiOblom <47597751+KrutoiOblom@users.noreply.github.com>
2019-04-24 12:52:05 +0300

    Add files via upload

---
+++ Практическая работа №8/Практическая работа №8.1/Практическая работа №8.1.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	
+	string word;
+	ifstream fin("infile.txt");
+	if (!fin.is_open()) {
+		cerr << "Не удалось открыть файл." << endl << endl;
+		system("pause");
+		return 1;
+	}
+	while (!fin.eof()) {
+		fin >> word;
+		cout << word << endl;
+	}
+	fin.close();
+	cout << endl;
+	system("pause");
+	return 0;
+}
---
+++ Практическая работа №8/Практическая работа №8.2/Практическая работа №8.2.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <windows.h>
+
+using namespace std;
+
+int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	ofstream fout("outfile.txt");
+	string a;
+	cout << "Введите текст, который желаете записать в файл" << endl;
+	getline(cin, a);
+	fout << a;
+	fout.close();
+	system("pause");
+	return 0;
+}
